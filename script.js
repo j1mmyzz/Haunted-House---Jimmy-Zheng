d8b77beb8a410e9cea6af6c9813d42906c863baa
j1mmyzz <jime82075@gmail.com>
2023-10-02 12:36:11 -0400

    js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-cards = [
-
-]
+cards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex > 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+  
+  shuffle(cards);
+  console.log(cards);
+  console.log("he")
 //make array then randomize then use .map to mao it to html
