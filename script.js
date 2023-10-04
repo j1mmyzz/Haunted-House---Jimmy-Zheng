889acf253fdca7e315b6e87c8e3e299b41db69d6
j1mmyzz <jime82075@gmail.com>
2023-10-04 12:30:41 -0400

    new idea

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 cards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+
+//shuffles the id of the cards
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
     while (currentIndex > 0) {
@@ -13,3 +15,10 @@ function shuffle(array) {
   shuffle(cards);
   console.log(cards);
 //make array then randomize then use .map to mao it to html
+
+allCards = document.getElementsByClassName("card");
+console.log(allCards);
+
+for(let i = 0; i < 16; i++){
+  console.log(allCards[i].getElementsByClassName)
+}
