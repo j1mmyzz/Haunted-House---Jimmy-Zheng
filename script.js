09b44b674a20b431a212fcb2c26255f62919738d
j1mmyzz <jime82075@gmail.com>
2023-10-06 12:01:18 -0400

    hjk

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-cards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
-
-//shuffles the id of the cards
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-    while (currentIndex > 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-    return array;
-  }
-  
-  shuffle(cards);
-  console.log(cards);
-//make array then randomize then use .map to mao it to html
-
+const checkClass = () => {
+  buttonClass = get;
+  console.log(buttonClass)
+}
